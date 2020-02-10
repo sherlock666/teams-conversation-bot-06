0d0ff5b9e4cc48a77ca5789d408bfd297dd0e499
Chevady <35552608+sherlock666@users.noreply.github.com>
2020-02-10 16:57:41 +0800

    Add files via upload

--- bots/teams_conversation_bot.py
+++ bots/teams_conversation_bot.py
@@ -38,140 +38,140 @@ class TeamsConversationBot(TeamsActivityHandler):
 
 ##########***天氣預報***#########  
 
-        if event.message.text == "!天氣預報":
+        if text == "!天氣預報":
             content = "請輸入欲查詢地點\n(目前限台灣本島+離島)\n\n使用方式如下(已設有防呆):\n!台北市\n!臺北市\n!台北\n!臺北\n!taipei"
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
 
-        if event.message.text == "!台北市" or event.message.text == "!臺北市" or event.message.text == "!台北" or event.message.text == "!臺北" or event.message.text == "!taipei":
+        if text == "!台北市" or text == "!臺北市" or text == "!台北" or text == "!臺北" or text == "!taipei":
             content = weather(L=0)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!新北市" or event.message.text == "!新北" or event.message.text == "!new taipei":
+        if text == "!新北市" or text == "!新北" or text == "!new taipei":
             content = weather(L=1)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!桃園市" or event.message.text == "!taoyuan":
+        if text == "!桃園市" or text == "!taoyuan":
             content = weather(L=2)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!台中市" or event.message.text == "!臺中市" or event.message.text == "!台中" or event.message.text == "!臺中" or event.message.text == "!taichung":
+        if text == "!台中市" or text == "!臺中市" or text == "!台中" or text == "!臺中" or text == "!taichung":
             content = weather(L=3)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!台南市" or event.message.text == "!臺南市" or event.message.text == "!台南" or event.message.text == "!臺南" or event.message.text == "!tainan":
+        if text == "!台南市" or text == "!臺南市" or text == "!台南" or text == "!臺南" or text == "!tainan":
             content = weather(L=4)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!高雄市" or event.message.text == "!高雄" or event.message.text == "!kaohsiung":
+        if text == "!高雄市" or text == "!高雄" or text == "!kaohsiung":
             content = weather(L=5)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!基隆市" or event.message.text == "!基隆" or event.message.text == "!keelung":
+        if text == "!基隆市" or text == "!基隆" or text == "!keelung":
             content = weather(L=6)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!新竹縣" or event.message.text == "!hsinchu county":
+        if text == "!新竹縣" or text == "!hsinchu county":
             content = weather(L=7)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!新竹市" or event.message.text == "!hsinchu city":
+        if text == "!新竹市" or text == "!hsinchu city":
             content = weather(L=8)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!苗栗縣" or event.message.text == "!苗栗" or event.message.text == "!miaoli":
+        if text == "!苗栗縣" or text == "!苗栗" or text == "!miaoli":
             content = weather(L=9)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!彰化縣" or event.message.text == "!彰化" or event.message.text == "!changhua":
+        if text == "!彰化縣" or text == "!彰化" or text == "!changhua":
             content = weather(L=10)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!南投縣" or event.message.text == "!南投" or event.message.text == "!nantou":
+        if text == "!南投縣" or text == "!南投" or text == "!nantou":
             content = weather(L=11)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!雲林縣" or event.message.text == "!雲林" or event.message.text == "!yunlin":
+        if text == "!雲林縣" or text == "!雲林" or text == "!yunlin":
             content = weather(L=12)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!嘉義縣" or event.message.text == "!chiayi county":
+        if text == "!嘉義縣" or text == "!chiayi county":
             content = weather(L=13)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!嘉義市" or event.message.text == "!chiayi city":
+        if text == "!嘉義市" or text == "!chiayi city":
             content = weather(L=14)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!屏東縣" or event.message.text == "!屏東" or event.message.text == "!pingtung":
+        if text == "!屏東縣" or text == "!屏東" or text == "!pingtung":
             content = weather(L=15)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!宜蘭縣" or event.message.text == "!宜蘭" or event.message.text == "!ilan":
+        if text == "!宜蘭縣" or text == "!宜蘭" or text == "!ilan":
             content = weather(L=16)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!花蓮縣" or event.message.text == "!花蓮" or event.message.text == "!hualien":
+        if text == "!花蓮縣" or text == "!花蓮" or text == "!hualien":
             content = weather(L=17)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!臺東縣" or event.message.text == "!台東" or event.message.text == "!taitung":
+        if text == "!臺東縣" or text == "!台東" or text == "!taitung":
             content = weather(L=18)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!澎湖縣" or event.message.text == "!澎湖" or event.message.text == "!penghu":
+        if text == "!澎湖縣" or text == "!澎湖" or text == "!penghu":
             content = weather(L=19)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!金門縣" or event.message.text == "!金門" or event.message.text == "!jinmen":
+        if text == "!金門縣" or text == "!金門" or text == "!jinmen":
             content = weather(L=20)
             await turn_context.send_activity(
              MessageFactory.text(content)
             )
             return 0
-        if event.message.text == "!連江縣" or event.message.text == "!連江" or event.message.text == "!lianjiang":
+        if text == "!連江縣" or text == "!連江" or text == "!lianjiang":
             content = weather(L=21)
             await turn_context.send_activity(
              MessageFactory.text(content)
